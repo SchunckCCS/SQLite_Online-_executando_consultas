73d04b63789c0007a0c83b9e2d95c8da16614249
SchunckCCS <125136575+SchunckCCS@users.noreply.github.com>
2026-03-12 18:45:13 -0300

    Exercícios do curso
    Adicionando os exercícios do curso que englobam todo o conteúdo passado.

---
+++ Botando_a_mão_na_massa.sql
@@ -0,0 +1,8 @@
+1) SELECT * from Colaboradores LIMIT 5
+
+2) SELECT * FROM Dependentes 
+WHERE genero != 'Masculino' AND parentesco = 'filha';
+
+3) SELECT * from Colaboradores WHERE nome like('A%')and nome like('%sa');
+
+4) SELECT cargo, salario from HistoricoEmprego where salario > 5000 
