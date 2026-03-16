8d900fb4762e2d29eb98709d6c10f5c76df803c1
SchunckCCS <125136575+SchunckCCS@users.noreply.github.com>
2026-03-16 18:09:54 -0300

    Add SQL queries to Botando_a_mão_na_massa.sql

--- Botando_a_mão_na_massa.sql
+++ Botando_a_mão_na_massa.sql
@@ -6,3 +6,5 @@ WHERE genero != 'Masculino' AND parentesco = 'filha';
 3) SELECT * from Colaboradores WHERE nome like('A%')and nome like('%sa');
 
 4) SELECT cargo, salario from HistoricoEmprego where salario > 5000 
+
+5) SELECT nome || ' ' || endereco as sla from Colaboradores;
